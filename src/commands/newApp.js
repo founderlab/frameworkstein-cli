bf9eabc1c448d3120a8758dc44cff90d45017895
Edward <hengji.liu@outlook.com>
2016-04-29 14:55:39 +1000

    use Queue to final clean up

--- src/commands/newApp.js
+++ src/commands/newApp.js
@@ -2,64 +2,59 @@ import fs from 'fs'
 import https from 'https'
 import unzip from 'unzip'
 import rimraf from 'rimraf'
+import Queue from 'queue-async'
 export default function newApp(_options, _callback) {
 
   const options = {
     name: _options.name,
     ..._options,
   }
-
   const zipFile = './' + options.name+'.zip'
   const oldFolder = options.name+'-master'
   const newFolder = options.name
   // redirected from https://github.com/founderlab/fl-cli/archive/master.zip
   const url = 'https://codeload.github.com/founderlab/'+options.name+'/zip/master'
-
-  function callback(_err) {
-    // check zip accessibility
-    fs.access(zipFile, fs.R_OK | fs.W_OK, err => {
-      if (err) return _callback(err)
-      fs.unlink(zipFile, err => {
-        if (err) return _callback(err)
-        console.log('--Zip deleted.')
-        return _callback(_err)
-      })
-    })
-  }
-
   const writer = fs.createWriteStream(zipFile)
 
-  // download
+  function callback() {
+    const queue = new Queue()
+    queue.defer(callback => {
+      fs.access(zipFile, fs.F_OK, err => {
+        if (err) return callback(err)
+        fs.unlink(zipFile, err => {
+          if (err) return callback(err)
+          console.log('--Zip deleted.')
+          return callback(null)
+        })
+      })
+    })
+    queue.defer(callback => {
+      fs.access(oldFolder, fs.F_OK, err => {
+        if (err) {// err means successfully renamed
+          console.log('--Folder renamed to '+ newFolder + '.')
+          return callback(null)
+        } // otherwise delete the old folder
+        rimraf(oldFolder, err => {
+          if (err) return callback(err)
+          console.log('--Folder ' + oldFolder + ' deleted.')
+          return callback(new Error('Failed when renaming '+ oldFolder + ' to ' + newFolder + ', ' + newFolder + ' already exists.'))
+        })
+      })
+    })
+    queue.await(_callback)
+  }
+
+  // download, unzip, rename
   https.get(url, res => {
     res.on('data', d => {
       writer.write(d)
     })
-
-    // unzip to directory named <name>
     res.on('end', () => {
       console.log('--Zip downloaded.')
       const stream = fs.createReadStream(zipFile).pipe(unzip.Extract({ path: './' }))
       stream.on('close', () => {
         console.log('--Zip extracted to folder '+ oldFolder + '.')
-        fs.rename(oldFolder, newFolder, err1 => {
-          if (err1) { // new folder exists, unlink folder fl-cli-master
-            fs.access(oldFolder, fs.R_OK | fs.W_OK, err2 => {
-              if (err2) return callback(err2)
-              rimraf(oldFolder, err3 => {
-                if (err3) return callback(err3)
-                console.log('--Folder ' + oldFolder +' deleted.')
-                return callback(err1)//successfully deleted oldFolder throw cant-rename error
-              })
-            })
-          }
-          else {
-            // when without the else block, if renaming fails,
-            // these 2 lines should not be executed, but somehow it does.
-            // so i add it into a else block
-            console.log('--Folder renamed to '+ newFolder + '.')
-            return callback(err1)
-          }
-        })
+        fs.rename(oldFolder, newFolder, callback)
       })
     })
   })
